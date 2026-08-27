bebcad94d2f5d4e5adb0f14dc69f508e49e81e44
Angga maulana andalas <maulangga74@gmail.com>
2026-08-27 11:25:16 +0700

    feat: export Auth Admin SDK instance from functions/src/admin.ts

--- functions/src/admin.ts
+++ functions/src/admin.ts
@@ -1,5 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+import { getAuth } from "firebase-admin/auth";
 
 export const adminApp = getApps().length ? getApp() : initializeApp();
 export const db = getFirestore(adminApp);
+export const auth = getAuth(adminApp);
