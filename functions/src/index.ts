b3b54fe97221d5c43a4fe01cb567ab28c0a8792a
Angga maulana andalas <maulangga74@gmail.com>
2026-08-27 11:48:00 +0700

    feat: export createUser, updateUser, resetUserPassword callables

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,9 @@ import { onCall } from "firebase-functions/v2/https";
 import { submitSubmissionHandler } from "./submitSubmission";
 import { reviewSubmissionHandler } from "./reviewSubmission";
 import { uploadFileHandler } from "./uploadFile";
+import { createUserHandler } from "./createUser";
+import { updateUserHandler } from "./updateUser";
+import { resetUserPasswordHandler } from "./resetUserPassword";
 
 export const submitSubmission = onCall((request) =>
   submitSubmissionHandler(request.data, { auth: request.auth ? { uid: request.auth.uid } : undefined })
@@ -16,4 +19,16 @@ export const reviewSubmission = onCall((request) =>
 // needs more headroom than the default 256MiB/60s callable limits.
 export const uploadFile = onCall({ memory: "512MiB", timeoutSeconds: 120 }, (request) =>
   uploadFileHandler(request.data, { auth: request.auth ? { uid: request.auth.uid } : undefined })
+);
+
+export const createUser = onCall((request) =>
+  createUserHandler(request.data, { auth: request.auth ? { uid: request.auth.uid } : undefined })
+);
+
+export const updateUser = onCall((request) =>
+  updateUserHandler(request.data, { auth: request.auth ? { uid: request.auth.uid } : undefined })
+);
+
+export const resetUserPassword = onCall((request) =>
+  resetUserPasswordHandler(request.data, { auth: request.auth ? { uid: request.auth.uid } : undefined })
 );
