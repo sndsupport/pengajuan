bf70e55640b1182e019926bd1ae1a899abfdcd9c
Angga maulana andalas <maulangga74@gmail.com>
2026-08-28 11:54:51 +0700

    chore: remove dead uploadFile callable now that uploads go directly to Drive

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,6 @@ import { onCall } from "firebase-functions/v2/https";
 import { onDocumentUpdated } from "firebase-functions/v2/firestore";
 import { submitSubmissionHandler } from "./submitSubmission";
 import { reviewSubmissionHandler } from "./reviewSubmission";
-import { uploadFileHandler } from "./uploadFile";
 import { createUserHandler } from "./createUser";
 import { updateUserHandler } from "./updateUser";
 import { resetUserPasswordHandler } from "./resetUserPassword";
@@ -16,13 +15,6 @@ export const submitSubmission = onCall((request) =>
 
 export const reviewSubmission = onCall((request) =>
   reviewSubmissionHandler(request.data, { auth: request.auth ? { uid: request.auth.uid } : undefined })
-);
-
-// Attachments/signatures arrive as base64 (a 10MB file is ~13MB of base64) and
-// are held in memory as a decoded Buffer while being streamed to Drive, so this
-// needs more headroom than the default 256MiB/60s callable limits.
-export const uploadFile = onCall({ memory: "512MiB", timeoutSeconds: 120 }, (request) =>
-  uploadFileHandler(request.data, { auth: request.auth ? { uid: request.auth.uid } : undefined })
 );
 
 export const createUser = onCall((request) =>
